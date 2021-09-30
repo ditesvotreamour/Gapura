857d0053df0f188a758b0619d5b4d92557e32d59
Eko Yudianto <ditesvotreamour@gmail.com>
2021-09-30 21:00:30 +0700

    Update init.js

--- js/init.js
+++ js/init.js
@@ -572,7 +572,7 @@ function edrea_tm_animate_text(){
 	var animateSpan			= jQuery('.edrea_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Edrea Kennedy", "Web Developer"],
+			strings: ["Menjual Koi", "GAPURA KOI"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
